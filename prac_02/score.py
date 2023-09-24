a42936c5770894c131ecb87bd09d709932efef9b
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-09-24 14:56:40 +1000

    Refactor score categories program to incorporate functions.

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,15 +1,29 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+Program to determine score status
 """
+import random
 
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    user_score = float(input("Enter score: "))
+    result = determine_result(user_score)
+    print(result)
+    random_score = random.randint(0, 100)
+    result = determine_result(random_score)
+    print(f"Random score's result: {result}")
+
+
+def determine_result(score):
+    """Return a different result depending on the value of the score."""
+    if score > 100 or score < 0:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
