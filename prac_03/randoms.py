78dc8e2cb0e38cc270370b9fba940f372c670451
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-09-29 10:47:14 +1000

    Add program that prints a random number.

---
+++ prac_03/randoms.py
@@ -0,0 +1,11 @@
+"""
+Produce a random number between 1 and 100 inclusive.
+"""
+import random
+
+
+def main():
+    print(random.uniform(1, 100))  # Assuming decimal numbers are included.
+
+
+main()
