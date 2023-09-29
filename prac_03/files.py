2eabe82b55a1a9f4a5f524e593142021a7176af0
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-09-29 11:57:39 +1000

    Add the first two responses for the Files exercise.

---
+++ prac_03/files.py
@@ -0,0 +1,19 @@
+"""
+Demonstrate multiple instances of reading and writing to files.
+"""
+
+
+# 1.
+# FILENAME = "name.txt"
+
+# name = input("Name: ")
+# out_file = open(FILENAME, "w")
+# print(name, file=out_file)
+# out_file.close()
+
+# 2.
+# FILENAME = "name.txt"  # Constant defined again "as if it were a separate program."
+# out_file = open(FILENAME, "r")
+# for line in out_file:
+#     print(f"Your name is {line}")
+# out_file.close()
