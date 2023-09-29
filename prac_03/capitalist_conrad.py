e2d97667e4b08e2adc397754bda38093ec217f5c
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-09-29 10:57:37 +1000

    Use number_of_days variable to show the days simulated.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -15,8 +15,9 @@ MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
+number_of_days = 1
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+print(f"$Starting price: {price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -32,4 +33,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    number_of_days += 1
