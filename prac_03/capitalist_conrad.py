b5d6b4e35d7d60d59780713b96287405fec7fdb9
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-09-29 11:09:33 +1000

    Incorporate writing the output to a file.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,10 +14,13 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+OUTPUT_FILE = "stock_price_simulation.txt"
 
 number_of_days = 1
 price = INITIAL_PRICE
 print(f"$Starting price: {price:,.2f}")
+
+out_file = open(OUTPUT_FILE, "w")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -33,5 +36,7 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
     number_of_days += 1
+
+out_file.close()
