5f8e9df6160dcac15e0e0067ebe68f398b7cf149
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-09-30 17:04:55 +1000

    Uncomment code for each response

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,29 +4,29 @@ Demonstrate multiple instances of reading and writing to files.
 
 
 # 1.
-# FILENAME = "name.txt"
+FILENAME = "name.txt"
 
-# name = input("Name: ")
-# out_file = open(FILENAME, "w")
-# print(name, file=out_file)
-# out_file.close()
+name = input("Name: ")
+out_file = open(FILENAME, "w")
+print(name, file=out_file)
+out_file.close()
 
 # 2.
-# FILENAME = "name.txt"  # Constant defined again "as if it were a separate program."
-#
-# in_file = open(FILENAME, "r")
-# for line in in_file:
-#     print(f"Your name is {line}")
-# in_file.close()
+FILENAME = "name.txt"  # Constant defined again "as if it were a separate program."
+
+in_file = open(FILENAME, "r")
+for line in in_file:
+    print(f"Your name is {line}")
+in_file.close()
 
 # 3.
-# FILENAME = "numbers.txt"
-#
-# in_file = open(FILENAME, "r")
-# value_one = int(in_file.readline())
-# value_two = int(in_file.readline())
-# in_file.close()
-# print(f"The sum of these two digits is: {value_one + value_two}")
+FILENAME = "numbers.txt"
+
+in_file = open(FILENAME, "r")
+value_one = int(in_file.readline())
+value_two = int(in_file.readline())
+in_file.close()
+print(f"The sum of these two digits is: {value_one + value_two}")
 
 # 4.
 FILENAME = "numbers.txt"
