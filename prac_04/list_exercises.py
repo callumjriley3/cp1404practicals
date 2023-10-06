2c3dfb8b22d6c17eb3a20a7f350e9cc0549db330
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-06 11:52:51 +1000

    Add security checker exercise and improve program docstring

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,4 +1,5 @@
-"""Program to store numbers in a list."""
+"""Program for various list exercises."""
+
 
 def main():
     """Get input for 5 numbers and store them in a list to print the first, last, min, max, and average."""
@@ -11,6 +12,27 @@ def main():
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = get_username()
+    access_status = determine_status(username, usernames)
+    print(f"Access {access_status}")
+
+
+def get_username():
+    """Return a username obtained from user"""
+    username = input("Username: ")
+    return username
+
+
+def determine_status(username, usernames):
+    """Return status (granted or denied) based on list of existing usernames."""
+    if username in usernames:
+        return "granted"
+    else:
+        return "denied"
 
 
 main()
