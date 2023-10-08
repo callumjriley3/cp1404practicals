66485f751df4ce029bcb6c9192945e9aba7b0bd6
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-08 15:15:58 +1000

    Add function to get a valid 'number_of_picks'

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,10 +9,18 @@ NUMBER_OF_NUMBERS = 6
 
 
 def main():
-    number_of_picks = get_number_of_picks()
+    number_of_picks = get_valid_number_of_picks()
     for i in range(number_of_picks):
         for j in range(NUMBER_OF_NUMBERS):
             print(generate_random_number(LOWER_LIMIT, UPPER_LIMIT))
+
+
+def get_valid_number_of_picks():
+    number_of_picks = int(input("How many quick picks? "))
+    while number_of_picks < 1:
+        print("Invalid input.")
+        number_of_picks = int(input("How many quick picks? "))
+    return number_of_picks
 
 
 main()
