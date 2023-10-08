db73647b68b4200d77d0d627ff50b744d8c6803f
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-08 16:04:58 +1000

    Add function docstrings and remove redundant random generation function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,28 +9,26 @@ NUMBER_OF_NUMBERS = 6
 
 
 def main():
+    """Obtain a number of picks and then, for that number of picks, generates and prints an amount of random numbers."""
     number_of_picks = get_valid_number_of_picks()
     for i in range(number_of_picks):
         random_numbers = []
         for j in range(NUMBER_OF_NUMBERS):
-            random_number = generate_random_number(LOWER_LIMIT, UPPER_LIMIT)
+            random_number = random.randint(LOWER_LIMIT, UPPER_LIMIT)
             while random_number in random_numbers:
-                random_number = generate_random_number(LOWER_LIMIT, UPPER_LIMIT)
+                random_number = random.randint(LOWER_LIMIT, UPPER_LIMIT)
             random_numbers.append(random_number)
             random_numbers.sort()
         print(" ".join(f"{random_number:2}" for random_number in random_numbers))
 
 
 def get_valid_number_of_picks():
+    """Return a number of quick picks as an integer greater than zero."""
     number_of_picks = int(input("How many quick picks? "))
     while number_of_picks < 1:
         print("Invalid input.")
         number_of_picks = int(input("How many quick picks? "))
     return number_of_picks
-
-
-def generate_random_number(lower_limit, upper_limit):
-    return random.randint(lower_limit, upper_limit)
 
 
 main()
