ce4e69f8181c6250599be4ad2351cf7baebc46e7
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-13 11:44:22 +1000

    Print words sorted alphabetically for prac 5 word occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -9,4 +9,5 @@ text = input("Text: ")
 for word in text.split():
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-print(word_to_count)
+for word in sorted(list(word_to_count)):
+    print(f"{word} : {word_to_count[word]}")
