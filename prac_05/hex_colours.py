b4e86bceea3485d04824e119d93ea80a2aad2425
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-13 11:21:16 +1000

    Complete hex colours exercise from prac 5

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,13 @@
+COLOUR_TO_CODE = {"aliceblue": "#f0f8ff", "barnred": "#7c0a02", "cornflowerblue": "#6495ed", "daffodil": "#ffff31",
+                  "emerald": "#50c878", "ferrarired": "#ff2800", "genericviridian": "#007f66",
+                  "hollywoodcerise": "#f400a1", "internationalorange": "#ff4f00", "jonquil": "#f4ca16"}
+
+colour = input("Enter colour name (without spaces): ").lower()
+while colour != "":
+    try:
+        print(f"The hexadecimal code for {colour} is: {COLOUR_TO_CODE[colour]}")
+        colour = input("Enter colour name (without spaces): ").lower()
+
+    except KeyError:
+        print("Invalid input.")
+        colour = input("Enter colour name (without spaces): ").lower()
