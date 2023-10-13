fe7680b2d2f903bfd3163333b40b71962b0dbb40
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-13 11:38:59 +1000

    Add loop to put words and number of occurrences in dictionary for prac 5 word occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,5 +1,12 @@
 """
 Word Occurrences
-Estimate:
+Estimate: 30 minutes
 Actual:
 """
+
+word_to_count = {}
+text = input("Text: ")
+for word in text.split():
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+print(word_to_count)
