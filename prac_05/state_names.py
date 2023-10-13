bd6d94abbd2ef3a212940b8ef641bb5c67d19f59
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-13 10:31:28 +1000

    Add for loop to print all states and names aligned

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,17 +1,21 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
 """
 
+MAX_SHORT_STATE_LENGTH = 3
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
+for pair in CODE_TO_NAME.items():
+    print(f"{pair[0]:{MAX_SHORT_STATE_LENGTH}} is {pair[1]}")
+
+
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+        print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
