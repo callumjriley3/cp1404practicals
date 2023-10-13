af17a2cd1608e8b3f6e97a3ceb0eed1a9a5d6cb3
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-13 11:23:48 +1000

    Add program docstring to hex colours program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,7 @@
+"""
+Program to display hexadecimal code for a colour.
+"""
+
 COLOUR_TO_CODE = {"aliceblue": "#f0f8ff", "barnred": "#7c0a02", "cornflowerblue": "#6495ed", "daffodil": "#ffff31",
                   "emerald": "#50c878", "ferrarired": "#ff2800", "genericviridian": "#007f66",
                   "hollywoodcerise": "#f400a1", "internationalorange": "#ff4f00", "jonquil": "#f4ca16"}
