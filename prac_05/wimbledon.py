a4ca2605cc9f50ce4d1fd9cfaafeab68edc0de11
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-15 09:55:40 +1000

    Implement for loop for set of countries in prac 5 wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,7 +9,11 @@ FILENAME = "wimbledon.csv"
 
 def main():
     champion_to_number_of_wins = {}
+    countries = set()
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
-
+        for line in in_file:
+            data = line.split(",")
+            countries.add(data[1])
+    print(countries)
 
 main()
