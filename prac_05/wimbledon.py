2f9357a785f953b5b14c052290b1587e0391c233
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-15 09:45:49 +1000

    Add main function to prac 5 wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,3 +3,13 @@ Wimbledon
 Estimate: 50 minutes
 Actual:
 """
+
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    champion_to_number_of_wins = {}
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+
+
+main()
