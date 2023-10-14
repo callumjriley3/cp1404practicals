a7e560b5074b609b8590c4adf41a46ba35b56573
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-14 16:24:09 +1000

    Changed user_email to email, added dictionary input for prac 5 emails

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,15 +7,18 @@ Actual:
 
 def main():
     email_to_name = {}
-    user_email = input("Email: ")
-    while user_email != "":
-        full_name = extract_name(user_email)
-        selection = input(f"Is your name {full_name}? (Y/n) ")
-        user_email = input("Email: ")
+    email = input("Email: ")
+    while email != "":
+        full_name = extract_name(email)
+        selection = input(f"Is your name {full_name}? (Y/n) ").upper()
+        if selection != "Y" and selection != "":
+            full_name = input("Name: ")
+        email_to_name[email] = full_name
+        email = input("Email: ")
 
 
-def extract_name(user_email):
-    parts = user_email.split("@")
+def extract_name(email):
+    parts = email.split("@")
     names = parts[0].split(".")
     full_name = " ".join(names).title()
     return full_name
