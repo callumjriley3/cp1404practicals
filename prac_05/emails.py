75a43aa9fdcc6a2edb0bd53c257484f91b471dbb
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-14 16:11:00 +1000

    Add name extraction for prac 5 emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,5 +1,23 @@
 """
 Emails
-Estimate:
+Estimate: 20 minutes
 Actual:
 """
+
+
+def main():
+    email_to_name = {}
+    user_email = input("Email: ")
+    while user_email != "":
+        name = extract_name(user_email)
+        print(name)
+        user_email = input("Email: ")
+
+
+def extract_name(user_email):
+    parts = user_email.split("@")
+    name = parts[0].split(".")
+    return name
+
+
+main()
