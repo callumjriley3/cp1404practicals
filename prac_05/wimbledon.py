0d5460661fbea4f852d199168e3fa1671be16755
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-15 10:14:01 +1000

    Extract functions for prac 5 wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -14,11 +14,25 @@ def main():
         for line in in_file:
             data = line.split(",")
             if data[2] != "Champion":
-                champion = data[2]
-                champion_to_number_of_wins[champion] = champion_to_number_of_wins.get(champion, 0) + 1
+                add_to_dictionary(champion_to_number_of_wins, data)
             if data[1] != "Country":
                 countries.add(data[1])
 
+    display_champions(champion_to_number_of_wins)
+    print()
+    display_countries(countries)
+
+
+def add_to_dictionary(champion_to_number_of_wins, data):
+    champion = data[2]
+    champion_to_number_of_wins[champion] = champion_to_number_of_wins.get(champion, 0) + 1
+
+def display_champions(champion_to_number_of_wins):
+    print("Wimbledon Champions: ")
+    for pair in champion_to_number_of_wins.items():
+        print(f"{pair[0]} {pair[1]}")
+
+def display_countries(countries):
     print("These 12 countries have won Wimbledon:")
     print(", ".join(sorted(countries)))
 
