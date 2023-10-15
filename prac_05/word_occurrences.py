f3439ab9da16c22b5216699f34f585a2294ddc27
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-15 20:57:42 +1000

    Change variable name max_word_length to maximum_word_length in prac 5 word occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,13 +4,13 @@ Estimate: 30 minutes
 Actual: 18 minutes
 """
 
-max_word_length = 0
+maximum_word_length = 0
 word_to_count = {}
 text = input("Text: ")
 for word in text.split():
     word_to_count[word] = word_to_count.get(word, 0) + 1
-    if len(word) > max_word_length:
-        max_word_length = len(word)
+    if len(word) > maximum_word_length:
+        maximum_word_length = len(word)
 
 for word in sorted(list(word_to_count)):
-    print(f"{word:{max_word_length}} : {word_to_count[word]}")
+    print(f"{word:{maximum_word_length}} : {word_to_count[word]}")
