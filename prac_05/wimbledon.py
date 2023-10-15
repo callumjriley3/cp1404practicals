03e4e37b74d40306223e553f48a048f5dbcf7c8b
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-15 10:18:07 +1000

    Add docstrings and actual working time for prac 5 wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,13 +1,14 @@
 """
 Wimbledon
 Estimate: 50 minutes
-Actual:
+Actual: 42 minutes
 """
 
 FILENAME = "wimbledon.csv"
 
 
 def main():
+    """Process data from file and display champions, number of wins and winning countries."""
     champion_to_number_of_wins = {}
     countries = set()
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
@@ -24,15 +25,20 @@ def main():
 
 
 def add_to_dictionary(champion_to_number_of_wins, data):
+    """Add champion name and how many times a win for that champion occurs to dictionary."""
     champion = data[2]
     champion_to_number_of_wins[champion] = champion_to_number_of_wins.get(champion, 0) + 1
 
+
 def display_champions(champion_to_number_of_wins):
+    """Print each champion and their number of wins."""
     print("Wimbledon Champions: ")
     for pair in champion_to_number_of_wins.items():
         print(f"{pair[0]} {pair[1]}")
 
+
 def display_countries(countries):
+    """Print winning countries in alphabetical order."""
     print("These 12 countries have won Wimbledon:")
     print(", ".join(sorted(countries)))
 
