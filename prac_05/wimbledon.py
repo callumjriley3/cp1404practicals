3395790624859a1b59a01aedc2d86fec53b04d7e
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-15 10:07:51 +1000

    Add champion and number of wins to dictionary in prac 5 wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -13,6 +13,9 @@ def main():
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         for line in in_file:
             data = line.split(",")
+            if data[2] != "Champion":
+                champion = data[2]
+                champion_to_number_of_wins[champion] = champion_to_number_of_wins.get(champion, 0) + 1
             if data[1] != "Country":
                 countries.add(data[1])
 
