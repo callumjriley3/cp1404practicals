066d202e0d0976d143d1d7c5da2dca53b0e7f0d4
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-15 21:05:26 +1000

    Change constant name MAX_SHORT_STATE_LENGTH to MAXIMUM_CODE_LENGTH in prac 5 state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -3,13 +3,13 @@ CP1404/CP5632 Practical
 State names in a dictionary
 """
 
-MAX_SHORT_STATE_LENGTH = 3
+MAXIMUM_CODE_LENGTH = 3
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
 
 for pair in CODE_TO_NAME.items():
-    print(f"{pair[0]:{MAX_SHORT_STATE_LENGTH}} is {pair[1]}")
+    print(f"{pair[0]:{MAXIMUM_CODE_LENGTH}} is {pair[1]}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
