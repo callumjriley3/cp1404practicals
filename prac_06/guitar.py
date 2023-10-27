27bd4f34be770368b20ca76a2179dd221ea74070
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-27 11:36:30 +1000

    Define __init__ method in guitar class program

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""
+Guitar class
+Estimated: 20 minutes
+Actual:
+"""
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
