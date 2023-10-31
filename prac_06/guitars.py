ad7697dc50de6ea5f1b415e88d3db155729394fb
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-10-31 14:51:03 +1000

    Incorporate ternary operator inside for loop in guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -24,11 +24,8 @@ def main():
 
     print("These are my guitars:")
     for i, guitar in enumerate(guitars, 1):
-        if guitar.is_vintage():
-            vintage_string = " (vintage)"
-        else:
-            vintage_string = ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+        vintage_string = "(vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
 
 
 main()
