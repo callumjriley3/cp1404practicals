61a8fe2a13a66f9c92f89bbd5592ea17bbfbb2d3
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-11-12 22:05:34 +1000

    Handle invalid inputs in prac 8 conversion programs

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,10 +3,11 @@ Miles to Kilometres Converter
 CP1404 Practical 8
 """
 
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
+
+CONVERSION_RATE = 1.60934
 
 
 class ConvertMilesKilometresApp(App):
@@ -22,10 +23,23 @@ class ConvertMilesKilometresApp(App):
     def handle_convert(self, value):
         """Handle conversion of value in miles to kilometres and output result to label."""
         try:
-            result = float(value) * 1.60934
+            result = float(value) * CONVERSION_RATE
             self.root.ids.output_label.text = str(result)
         except ValueError:
-            pass
+            self.root.ids.output_label.text = str(0.0)
+
+    def handle_increment(self, text, increment_amount):
+        """Handle incrementation of value in text box based on amount passed in."""
+        value = self.convert_to_float(text) + increment_amount
+        self.root.ids.input_number.text = str(value)
+
+    @staticmethod
+    def convert_to_float(text):
+        """Convert text to float, returning 0.0 in case of error."""
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
 
 
 ConvertMilesKilometresApp().run()
