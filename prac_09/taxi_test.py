87033900881a285df1488e1d7527d77a7bb36ced
callumjriley3 <callum.riley@my.jcu.edu.au>
2023-11-17 10:20:06 +1000

    Complete last part of testing Taxi class for prac 9 and change Taxi to use class variable

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,7 +7,7 @@ from prac_09.taxi import Taxi
 
 def run_tests():
     """Run a series of tests to demonstrate the Taxi class' methods."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
 
     my_taxi.drive(40)
 
